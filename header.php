723758c13b649feb39e849ba72c09946269302eb
Amin Alsaeed <6059169@mborijnland.nl>
2026-03-17 15:02:46 +0100

    header.php bestand gemaakt

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Amin">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Vegan Restaurant</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/index.js" defer></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap" rel="stylesheet">
+</head>
+<body>
+<!--Dit is branch AMIN-->
+    <header class="header">
+        <h2 class="reserveer-nummer"> <span class="reserveer">Reserveer nu:</span> 020 456 78 92</h2>
+        <hr class="hr-header">
+        <article class="icons-logo">
+              <img class="logo-img" src="/logo_vegan_food_amsterdam.png" alt="Logo-Img">
+              <div class="icons">
+                  <span class="material-symbols-outlined">search</span>
+                  <span class="material-symbols-outlined">menu</span>
+              </div>
+            
+        </article>
+      
+
+    </header>
+
+    <nav class="nav-bar">
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Lunch-Diner</a></li>
+            <li><a href="#">Reserveren</a></li>
+            <li><a href="#">Openingstijden</a></li>
+            <li><a href="#">Vacatures</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section class="fotoGallerie-sec">
+            <div class="fotoGallerie">
+                <img class="foto1" src="/imgs/89aaa4fe-6f7e-447e-992a-aaa20923934a.avif" alt="etenFoto">
+                <img class="foto2" src="" alt="etenFoto">
+                <img class="foto3" src="" alt="etenFoto">
+                <img class="foto4" src="" alt="etenFoto">
+
+            </div>
+            
+        </section>
+    </main>
+
+</body>
+</html>
