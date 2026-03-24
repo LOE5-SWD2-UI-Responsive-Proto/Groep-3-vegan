2cbe2c46c2a26f253ab73c6ee166ea0b66427fc7
Evra Mekbeb <6047287@mborijnland.nl>
2026-03-24 15:25:01 +0100

    light darkmode functie gegeven

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-let lightdark;
+const knop = document.getElementById("lightdark") && document.getElementsByClassName("openingstijden");
+
+function switchlightdark() {
+    if (document.documentElement.dataset.mode != 'dark') {
+        document.documentElement.dataset.mode = 'dark';
+    } else {
+        document.documentElement.dataset.mode = 'light';
+    }
+}
+
+lightdark.addEventListener('click', switchlightdark);
