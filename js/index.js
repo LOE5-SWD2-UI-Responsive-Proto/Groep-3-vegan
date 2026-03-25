1d474cad1bcf2562c5ee971ca355edc6e29cdbfe
Evra Mekbeb <6047287@mborijnland.nl>
2026-03-25 10:36:01 +0100

    test

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const knop = document.getElementById("lightdark") && document.getElementsByClassName("openingstijden");
+//light dark mode//
+
+const zwartWit = document.getElementById("lightdark");
 
 function switchlightdark() {
     if (document.documentElement.dataset.mode != 'dark') {
@@ -9,3 +11,38 @@ function switchlightdark() {
 }
 
 lightdark.addEventListener('click', switchlightdark);
+
+
+// open close code
+
+let objDate = new Date();
+
+const openOrClosed = document.getElementById("openOrClosed");
+
+function openOrClosedMessage() {
+    const hour = objDate.getHours();
+    const minutes = objDate.getMinutes();
+
+    if (hour >= 17 && hour <= 22) {
+        openOrClosed.style.color = 'green';
+
+        if (60 - minutes <= 10) {
+            openOrClosed.innerHTML = `Open - Closes in: ${22 - hour}:${60 - minutes}`;
+        } else {
+            openOrClosed.innerHTML = `Open - Closes in: ${22 - hour}:${60 - minutes}`;
+        }
+
+    } else {
+        openOrClosed.style.color = 'red';
+
+        if (hour >= 22) {
+            openOrClosed.innerHTML = `Closed - Opens in: ${24 - hour + 12}`;
+        } else {
+            openOrClosed.innerHTML = `Closed - Opens in: ${12 - hour}`;
+        }
+    }
+}
+function week() {
+    openOrClosedMessage();
+}
+openOrClosedMessage();
