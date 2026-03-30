1fdbf16ea839cd957c02f4c30427f34b7878e03e
Amin Alsaeed <6059169@mborijnland.nl>
2026-03-30 12:26:29 +0200

    css aangepast slide show gemaakt html aangepast imgs toegevoegd

--- js/index.js
+++ js/index.js
@@ -8,3 +8,35 @@ function toggleNav() {
 
 hamburger.addEventListener('click', toggleNav);
 closeBtn.addEventListener('click', toggleNav);
+
+
+let current = 1;
+const total = 4;
+
+const fotos = [
+  document.querySelector(".foto1"),
+  document.querySelector(".foto2"),
+  document.querySelector(".foto3"),
+  document.querySelector(".foto4")
+];
+
+function showFoto(n) {
+  fotos.forEach(f => f.style.display = "none");
+  fotos[n - 1].style.display = "block";
+}
+
+function nextSlide() {
+  current = (current % total) + 1;
+  showFoto(current);
+}
+
+function prevSlide() {
+  current = (current - 2 + total) % total + 1;
+  showFoto(current);
+}
+
+document.querySelector(".forward").addEventListener("click", nextSlide);
+document.querySelector(".back").addEventListener("click", prevSlide);
+
+showFoto(current);
+setInterval(nextSlide, 4000);
