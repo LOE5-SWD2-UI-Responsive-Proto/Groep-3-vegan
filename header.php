937a8aae72cc33f700a93a3493d12a30519615ef
Amin Alsaeed <6059169@mborijnland.nl>
2026-03-31 15:38:26 +0200

    kleine aanpassingen

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
     <title>Vegan Restaurant</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/index.js" defer></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/index.js" defer></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap"
         rel="stylesheet">
@@ -25,7 +25,7 @@
         <h2 class="reserveer-nummer"> <span class="reserveer">Reserveer nu:</span> 020 456 78 92</h2>
         <hr class="hr-header">
         <article class="icons-logo">
-            <img class="logo-img" src="/logo_vegan_food_amsterdam.png" alt="Logo-Img">
+            <img class="logo-img" src="../logo_vegan_food_amsterdam.png" alt="Logo-Img">
             
             <nav class="nav-bar" id="nav">
         <ul class="menu">
--- html/index.php
+++ html/index.php
@@ -9,8 +9,8 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
     <title>Vegan Restaurant</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/index.js" defer></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/index.js" defer></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap"
         rel="stylesheet">
@@ -25,7 +25,7 @@
         <h2 class="reserveer-nummer"> <span class="reserveer">Reserveer nu:</span> 020 456 78 92</h2>
         <hr class="hr-header">
         <article class="icons-logo">
-            <img class="logo-img" src="/logo_vegan_food_amsterdam.png" alt="Logo-Img">
+            <img class="logo-img" src="../logo_vegan_food_amsterdam.png" alt="Logo-Img">
             
             <nav class="nav-bar" id="nav">
         <ul class="menu">
@@ -52,10 +52,10 @@
         <section class="fotoGallerie-sec">
             <div class="fotoGallerie">
 
-                <img class="foto1" src="/imgs/89aaa4fe-6f7e-447e-992a-aaa20923934a.avif" alt="etenFoto">
-                <img class="foto2" src="/imgs/download.png" alt="etenFoto">
-                <img class="foto3" src="/imgs/PixelBin-AI-Editor-1774865220778.png" alt="etenFoto">
-                <img class="foto4" src="/imgs/resized_no_padding_1920x1285.png" alt="etenFoto">
+                <img class="foto1" src="../imgs/89aaa4fe-6f7e-447e-992a-aaa20923934a.avif" alt="etenFoto">
+                <img class="foto2" src="../imgs/download.png" alt="etenFoto">
+                <img class="foto3" src="../imgs/PixelBin-AI-Editor-1774865220778.png" alt="etenFoto">
+                <img class="foto4" src="../imgs/resized_no_padding_1920x1285.png" alt="etenFoto">
                 <div class="slogan">
                     <span class="big">Vegan</span>
                     <span class="small edited">Vegan & Vegetarian Dining in</span>
@@ -78,7 +78,7 @@
         </section>
         <section class="team-sec">
             <div class="overOns-container">
-                <img class="team-foto" src="/imgs/afbeelding (2).png" alt="team foto">
+                <img class="team-foto" src="../imgs/afbeelding (2).png" alt="team foto">
                 <p class="overOns">Welkom bij ons groene paradijs in het hart van Amsterdam, waar passie voor de
                     plant-based keuken centraal staat. Laat je verrassen door onze luxe,
                     kleurrijke gerechten die met liefde en verse lokale ingrediënten zijn bereid. Ons hele team staat
