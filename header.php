561818d2bdcc2704cf93814050c8b336c075229c
Amin Alsaeed <6059169@mborijnland.nl>
2026-03-31 15:00:29 +0200

    paginas van html naar php gedaan

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,47 +12,40 @@
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/index.js" defer></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
-<!--Dit is branch AMIN-->
     <header class="header">
         <h2 class="reserveer-nummer"> <span class="reserveer">Reserveer nu:</span> 020 456 78 92</h2>
         <hr class="hr-header">
         <article class="icons-logo">
-              <img class="logo-img" src="/logo_vegan_food_amsterdam.png" alt="Logo-Img">
-              <div class="icons">
-                  <span class="material-symbols-outlined">search</span>
-                  <span class="material-symbols-outlined">menu</span>
-              </div>
+            <img class="logo-img" src="/logo_vegan_food_amsterdam.png" alt="Logo-Img">
             
-        </article>
-      
-
-    </header>
-
-    <nav class="nav-bar">
+            <nav class="nav-bar" id="nav">
         <ul class="menu">
-            <li><a href="#">Home</a></li>
+            <button class="close-btn">&times;</button>
+            <li><a href="index.html">Home</a></li>
             <li><a href="#">Lunch-Diner</a></li>
-            <li><a href="#">Reserveren</a></li>
-            <li><a href="#">Openingstijden</a></li>
-            <li><a href="#">Vacatures</a></li>
+            <li><a href="boeken.html">Reserveren</a></li>
+            <li><a href="opening en locatie.html">Openingstijden</a></li>
+            <li><a href="vacature.html">Vacatures</a></li>
         </ul>
     </nav>
-
-    <main>
-        <section class="fotoGallerie-sec">
-            <div class="fotoGallerie">
-                <img class="foto1" src="/imgs/89aaa4fe-6f7e-447e-992a-aaa20923934a.avif" alt="etenFoto">
-                <img class="foto2" src="" alt="etenFoto">
-                <img class="foto3" src="" alt="etenFoto">
-                <img class="foto4" src="" alt="etenFoto">
-
+    <div class="icons">
+                <span class="material-symbols-outlined" id="search">search</span>
+                <span class="material-symbols-outlined" id="hamburger">menu</span>
             </div>
-            
-        </section>
-    </main>
+        </article>
 
+
+    
+
+    </header>
 </body>
+
 </html>
